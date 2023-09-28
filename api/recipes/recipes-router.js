884923cd2eff45013bf1c3cf3971ab8e01ac6bd3
Jose Cepero <ceperojose54@gmail.com>
2023-09-28 19:07:32 -0400

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
 
-router.get('/',(req, res) =>{
-    res.json('router up and running')
-})
 
+router.use('*', (req,res) =>{
+    res.json('recipe api router up and running')
+})
 
 router.use((err, req, res, next) =>{ //eslint-disable-line
     res.status(err.status || 500).json({
